c5ef6ca43698a93e776ce52041517ba537a24c7b
rchaput <rchaput.pro@gmail.com>
2021-12-09 17:41:40 +0100

    [WIP] Refactored the agent's profiles
    Instead of having a '_load_profile' function in the 'scenarios' package (and no 'save' function), the notion of profile was refactored in a Profile class, in the 'util' package.
    This makes Profile safer (by controlling how to save and load), and easier to use (since we now have attributes instead of accessing a dictionary).

--- smartgrid/scenarios/_load_agent.py
+++ smartgrid/scenarios/_load_agent.py
@@ -49,64 +49,10 @@ _compute_production
 """
 
 from functools import partial
-from pathlib import Path
-
-import numpy as np
-from gym import spaces
 
 from smartgrid.agent import Agent
 from smartgrid.util import comforts
-
-
-def _load_profile(profile_path):
-    """Parse and validate an agent profile from a `.npz` file.
-
-    If the profile does not contain the expected data, or they do not
-    meet their constraints (specifically, in terms of shapes), this
-    will raise exceptions.
-    See this module's documentation for the expected data format.
-    """
-    # FIXME: replace the `raise()` calls by real exceptions
-    # First, create the "static" map to memorize profiles
-    # (this allows avoiding to reload and reparse the file between
-    # multiple invocations)
-    if not hasattr(_load_profile, 'memorized'):
-        _load_profile.memorized = {}
-    # If the profile was already parsed, return it immediately
-    if profile_path in _load_profile.memorized.keys():
-        return _load_profile.memorized[profile_path]
-    # Else, load the profile from the `npz` file (we will then memorize it)
-    with np.load(profile_path) as profile:
-        # Check if profile is correctly formatted
-        for key in ['action_space_low', 'action_space_high', 'max_storage',
-                    'need_per_hour', 'production_per_hour']:
-            if key not in profile.files:
-                raise('Profile incorrectly formatted! Missing element: ' + key)
-        # Parse the profile
-        action_space_low = profile['action_space_low']
-        action_space_high = profile['action_space_high']
-        if action_space_low.shape != action_space_high.shape:
-            raise('low and high bounds of action space do not have same shape!')
-        action_space = spaces.Box(
-            low=action_space_low,
-            high=action_space_high,
-            dtype=np.int
-        )
-        # .npz file only store arrays, we want `max_storage` as a single value
-        max_storage = profile['max_storage'][0]
-        need_per_hour = profile['need_per_hour']
-        production_per_hour = profile['production_per_hour']
-        if need_per_hour.shape != production_per_hour.shape:
-            raise('Need and production per hour do not have the same shape!')
-        # Memorize the parsed profile
-        parsed_profile = {
-            'action_space': action_space,
-            'max_storage': max_storage,
-            'need_per_hour': need_per_hour,
-            'production_per_hour': production_per_hour
-        }
-        _load_profile.memorized[profile_path] = parsed_profile
-        return parsed_profile
+from smartgrid.util import Profile
 
 
 def _compute_need(step, need_per_hour):
@@ -119,15 +65,15 @@ def _compute_production(step, production_per_hour):
     return production_per_hour[step]
 
 
-def _create_agent(name, comfort_fn, profile_filepath):
-    profile = _load_profile(profile_filepath)
+def _create_agent(name, comfort_fn, profile_filename):
+    profile = Profile.load(profile_filename)
 
-    action_space = profile['action_space']
-    max_storage = profile['max_storage']
+    action_space = profile.action_space
+    max_storage = profile.max_storage
     compute_need = partial(_compute_need,
-                           need_per_hour=profile['need_per_hour'])
+                           need_per_hour=profile.need_per_hour)
     compute_production = partial(_compute_production,
-                                 production_per_hour=profile['production_per_hour'])
+                                 production_per_hour=profile.production_per_hour)
 
     agent = Agent(name,
                   action_space,
@@ -140,8 +86,7 @@ def _create_agent(name, comfort_fn, profile_filepath):
 
 
 def create_household(i):
-    profile_filepath = Path(__file__) / '../../../data' / 'test_profile.npz'
-    profile_filepath = profile_filepath.resolve()
+    profile_filename = 'test_profile.npz'
     comfort_fn = comforts.comfort_flexible
     name = f'Household[Flexible] {i}'
-    return _create_agent(name, comfort_fn, profile_filepath)
+    return _create_agent(name, comfort_fn, profile_filename)
---
+++ smartgrid/util/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package contains various "utilities" functions (or helpers).
+"""
+
+from .agent_profile import Profile
+from .bounded import increase_bounded, decrease_bounded
+from .comforts import comfort_flexible, comfort_neutral, comfort_strict
+from .equity import hoover
