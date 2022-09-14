3ad26cba9e08cedfc489b48666c7835c3776994f
rchaput <rchaput.pro@gmail.com>
2022-09-14 15:42:10 +0200

    Added docs for 'observations'
    * Added a module doc.
    * Added doc for classes 'GlobalObservation', 'LocalObservation', 'Observation', 'ObservationManager', and their methods.
    * Added imports in the module directly to simplify imports from other modules.

--- smartgrid/observation/local_observation.py
+++ smartgrid/observation/local_observation.py
@@ -11,16 +11,50 @@ local_field = [
 
 class LocalObservation(namedtuple('LocalObservation', local_field)):
     """
-    Observation for a single Agent, it contains:
-        - personal_storage: the amount of energy in the battery.
-        - comfort: represent the fulfilled of his need with a Richard_curve
-        - payoff: money earn in selling energy.
+    Observations that are local (individual) to a single Agent.
+
+    These observations are not shared with other agents, and contain the
+    following measures:
+
+    personal_storage
+        The amount of energy currently available in the :py:class:`.Agent`'s
+        personal battery.
+        This amount is represented as a ratio between 0 (empty) and 1 (full),
+        w.r.t. the Agent's battery capacity. See :py:attr:`.Agent.storage_ratio`
+        for details.
+
+    comfort
+        This represents to which degree the agent satisfied its need by
+        consuming energy. Intuitively, the more an agent's consumption is
+        close to its need, the closer the comfort will be to 1. Conversely,
+        if an agent does not consume, its comfort will tend towards 0.
+        Comfort is computed through the Agent's comfort function; we describe
+        several examples in the :py:mod:`.comfort` module, which rely on
+        *generalized logistic curves* (similar to a sigmoid).
+
+    payoff
+        The agent's current amount of money. Money can be won by selling
+        energy from the personal battery to the national grid, or lost by
+        buying money from the national grid to the personal battery.
+        The payoff observation is interpolated from the agent's real payoff
+        and the payoff range to obtain a value between 0 (a loss) and 1 (a win),
+        with 0.5 being the neutral value (neither win nor loss).
     """
 
     @classmethod
     def compute(cls, agent: Agent):
         """
-        Compute all metric for the local observation.
+        Return local observations for a single agent.
+
+        This function extracts the relevant measures from an :py:class:`.Agent`.
+        Most of the computing has already been done in the
+        :py:meth:`.Agent.update` and :py:meth:`.Agent.handle_action` methods.
+
+        :type agent: smartgrid.agent.Agent
+        :param agent: The Agent for which we want to compute the local
+            observations.
+
+        :rtype: LocalObservation
         """
         # Individual data
         personal_storage = agent.storage_ratio
--- smartgrid/observation/observation_manager.py
+++ smartgrid/observation/observation_manager.py
@@ -8,33 +8,75 @@ from smartgrid.observation.local_observation import LocalObservation
 
 class ObservationManager:
     """
-    ObservationManager is a handler of the computation of the observation for one Agent.
-    It's fusion global and local observation.
-    """
-    global_observation: Type[GlobalObservation]
-    local_observation: Type[LocalObservation]
+    The ObservationManager is responsible for computing observations.
 
-    def __init__(self, local_observation: Type[LocalObservation], global_observation: Type[GlobalObservation]):
+    Its primary purpose is to allow extensibility: the attributes
+    :py:attr:`.global_observation` and :py:attr:`.local_observation`, which
+    are set through the constructor, control which Observation classes will
+    be used in the simulator. It is thus possible to subclass
+    :py:class:`.GlobalObservation` and/or :py:class:`.LocalObservation` to
+    use different observations.
+
+    The computing calls (:py:meth:`.compute_agent` and :py:meth:`.compute_global`)
+    are delegated to the corresponding calls through these attributes.
+    """
+
+    global_observation: Type[GlobalObservation]
+    """
+    The class that will be used to compute global observations.
+    It should be a subclass of :py:class:`.GlobalObservation` to ensure that
+    necessary methods are present.
+    Please note that this field should be set to a *class* itself, not an
+    instance, e.g., ``GlobalObservation`` (instead of ``GlobalObservation()``).
+    """
+
+    local_observation: Type[LocalObservation]
+    """
+    The class that will be used to compute local observations.
+    It should be a subclass of :py:class:`.LocalObservation` to ensure that
+    necessary methods are present.
+    Please note that this field should be set to a *class* itself, not an
+    instance, e.g., ``LocalObservation`` (instead of ``LocalObservation()``).
+    """
+
+    def __init__(self,
+                 local_observation: Type[LocalObservation],
+                 global_observation: Type[GlobalObservation]):
         self.global_observation = global_observation
         self.local_observation = local_observation
 
-    @abstractmethod
     def compute_agent(self, world: 'World', agent: Agent) -> LocalObservation:
         """
-        Create the observation for an Agent.
-        :param world: use for global and local observation
-        :param agent: use for local observation
+        Create the local observation for an Agent.
         """
         return self.local_observation.compute(agent)
 
     def compute_global(self, world) -> GlobalObservation:
+        """
+        Create the global observation for the World.
+        """
         return self.global_observation.compute(world)
 
     @property
     def shape(self) -> Dict[str, int]:
+        """
+        Describe the shapes of the various Observations (local, global, merged).
+
+        :rtype: dict
+        :return: A dict comprised of: ``agent_state``, ``local_state``, and
+            ``global_state``. Each of these fields describe the shape (i.e.,
+            number of dimensions) of the corresponding observation. Note that
+            ``agent_state`` refers to the merged (both local and global) case.
+        """
         return {"agent_state": len(self.local_observation._fields) + len(self.global_observation._fields),
                 "local_state": len(self.local_observation._fields),
                 "global_state": len(self.global_observation._fields)}
 
     def reset(self):
+        """
+        Reset the ObservationManager.
+
+        It is particularly important to reset the memoization process of
+        :py:class:`.GlobalObservation`.
+        """
         self.global_observation.reset()
